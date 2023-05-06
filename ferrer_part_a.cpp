e3201187e8396c5d9f7797df958e65ddcfa3652d
anushkav1888 <126143364+anushkav1888@users.noreply.github.com>
2023-05-07 01:47:18 +0530

    Add files via upload

---
+++ ferrer_part_a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+string A;
+cin>>A;
+for(int i=0;i<(sizeof(A)/4);i++)
+  { for(int j=0;j<A(i);j++)
+
+        {
+                cout<<"*"<<endl;
+
+
+        }
+
+
+
+  }
+
+
+
+
+
+
+}

f62e190f6ba38a511b31ed70a4b5db6184a2cbdb
anushkav1888 <126143364+anushkav1888@users.noreply.github.com>
2023-08-06 11:10:59 +0530

    week 1

--- ferrer_part_a.cpp
+++ ferrer_part_a.cpp
@@ -3,24 +3,20 @@ using namespace std;
 
 int main()
 {
-string A;
-cin>>A;
-for(int i=0;i<(sizeof(A)/4);i++)
-  { for(int j=0;j<A(i);j++)
+  string line;
+  vector<int> row;
+  getline(cin, line);
 
-        {
-                cout<<"*"<<endl;
-
-
-        }
-
-
-
+  for( char c: line){
+   if(c != ' '){ 
+     int a = c - '0';
+     row.push_back(a); 
+           }
   }
-
-
-
-
-
-
+for(int i = 0; i < row.size(); i++){
+  for(int j = 0; j < row[i]; j++){
+    cout<<"*";
+  }
+  cout<<endl;
+ }
 }
